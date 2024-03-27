0f0fa01f3cddf525fd70a195baca4c595bc18c2b
mit <mit@gccompany.co.kr>
2024-03-27 13:22:02 +0900

    gtm_log.csv 파일 로드 추가

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import pandas as pd 
+
 print("mit babo")
 print("data science team is best")
 print("여기어때")
 print("여기좋아")
 print("파마센")
 print("놀아죠")
+
+df = pd.read_csv('/Volumes/raw_db/datascience_ds/mit_test/gtm_log.csv', index_col=0)
+print(df)
