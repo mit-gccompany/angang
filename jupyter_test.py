d0d2622d67ecd6d40aa683db4b97f38587431693
mit <mit@gccompany.co.kr>
2024-04-04 12:44:12 +0900

    코드 수정

--- jupyter_test.py
+++ jupyter_test.py
@@ -4,3 +4,12 @@ print("done")
 
 # COMMAND ----------
 print("finish")
+
+
+# COMMAND ----------
+a = 1
+
+# COMMAND ----------
+a+3
+
+# COMMAND ----------
--- run.py
+++ run.py
@@ -6,6 +6,4 @@ print("여기어때")
 print("여기좋아")
 print("파마센")
 print("놀아죠")
-
-df = pd.read_csv('/Volumes/raw_db/datascience_ds/mit_test/gtm_log.csv', index_col=0)
-print(df)
+print("안녕")
